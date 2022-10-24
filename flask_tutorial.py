3b8e0db56ca15d35ece41ed3523dab8913ed2e79
andrewMasiero <andymasiero@gmail.com>
2022-10-24 17:00:13 -0500

    Middle of tutorial 7

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -1,13 +1,62 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.secret_key = 'hello'
+app.permanent_session_lifetime = timedelta(days=5)
+
+db = SQLAlchemy()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=["Bill", "Joe", "Earl"])
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        session.permanent = True
+        user = request.form['nm']
+        session['user'] = user
+        flash('Login Successful!')
+        return redirect(url_for('user'))
+    else:
+        if 'user' in session:
+            flash('Already Logged In!')
+            return redirect(url_for('user'))
+        return render_template('login.html')
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/user', methods=['POST', 'GET'])
+def user():
+    email = None
+    if 'user' in session:
+        user = session['user']
+
+        if request.method == 'POST':
+            email = request.form['email']
+            session['email'] = email
+        else:
+            if 'email' in session:
+                email = session['email']
+        return render_template('user.html', email=email)
+    else:
+        flash('You are not logged in!')
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        user = session['user']
+        flash(f'You have been logged out, {user}!', 'info')
+    
+    session.pop('user', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
