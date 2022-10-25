cb6dc195014c54d6d8b11a9c250da0863f7f7672
andrewMasiero <andymasiero@gmail.com>
2022-10-25 10:57:05 -0500

    stuck on sqlalchemy code

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -4,9 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'hello'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(days=5)
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 
 @app.route('/')
 def home():
@@ -19,6 +32,15 @@ def login():
         session.permanent = True
         user = request.form['nm']
         session['user'] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session['email'] = found_user.email
+        else:
+            usr = users(user, '')
+            db.session.add(usr)
+            db.commit()
+
         flash('Login Successful!')
         return redirect(url_for('user'))
     else:
@@ -37,6 +59,10 @@ def user():
         if request.method == 'POST':
             email = request.form['email']
             session['email'] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.commit()
+            flash('Email was saved!')
         else:
             if 'email' in session:
                 email = session['email']
@@ -51,12 +77,12 @@ def logout():
     if 'user' in session:
         user = session['user']
         flash(f'You have been logged out, {user}!', 'info')
-    
+
     session.pop('user', None)
     session.pop('email', None)
     return redirect(url_for('login'))
 
 
-
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
