06e2edf900daa3e50e75014ce6d4f2202a0984af
andrewMasiero <andymasiero@gmail.com>
2022-11-02 16:57:58 -0500

    sqlalchemy attribute error

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,9 +1,24 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash, get_flashed_messages
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Module):
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(100))
+    email = db.Column('email', db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 
 @app.route('/')
@@ -26,11 +41,20 @@ def login():
         return render_template('login.html')
 
 
-@app.route('/user')
+@app.route('/user', methods=['POST', 'GET'])
 def user():
+    email = None
     if 'user' in session:
         user = session['user']
-        return render_template('user.html', user=user)
+
+        if request.method == 'POST':
+            email = request.form['email']
+            session['email'] = email
+            flash('Email was saved.', 'info')
+        elif 'email' in session:
+            email = session['email']
+
+        return render_template('user.html', email=email)
     else:
         flash('You are not logged in', 'info')
         return redirect(url_for('login'))
@@ -39,9 +63,12 @@ def user():
 @app.route('/logout')
 def logout():
     session.pop('user', None)
+    session.pop('email', None)
     flash('You have been logged out', 'info')
     return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
