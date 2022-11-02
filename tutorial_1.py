f5ef1a0aca4e61372d43b34f7e647882035c5b6e
andrewMasiero <andymasiero@gmail.com>
2022-11-02 16:12:54 -0500

    message flashing

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash, get_flashed_messages
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = 'dev'
-app.permanent_session_lifetime = timedelta(seconds=5)
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 
 @app.route('/')
@@ -17,9 +17,11 @@ def login():
         session.permanent = True
         user = request.form['nm']
         session['user'] = user
+        flash('Login successful', 'info')
         return redirect(url_for('user', usr=user))
     else:
         if 'user' in session:
+            flash('Already logged in', 'info')
             return redirect(url_for('user'))
         return render_template('login.html')
 
@@ -28,14 +30,16 @@ def login():
 def user():
     if 'user' in session:
         user = session['user']
-        return f"<h1>{user}</h1>"
+        return render_template('user.html', user=user)
     else:
+        flash('You are not logged in', 'info')
         return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
     session.pop('user', None)
+    flash('You have been logged out', 'info')
     return redirect(url_for('login'))
 
 
